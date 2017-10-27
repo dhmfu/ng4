048934cd7eba04c2a3d553f9335cfe462d9c936d
Дудка Василий <dudka.vasia2010@gmail.com>
2017-10-27 12:55:19 +0300

    refactored styles of main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import * as io from 'socket.io-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
     constructor(private router: Router){}
